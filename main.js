c7aa367f2caf192274e4a8d5c2f388b552afce33
andiz95 <andizd95@gmail.com>
2021-01-21 12:02:50 +0700

    basic javascript

---
+++ main.js
@@ -0,0 +1,29 @@
+let anggota= {
+    nama: "Andi Darmawan",
+    alamat:"Yogyakarta",
+    hobby:"Badminton",
+    jenis_kelamin: "Laki-laki",
+    pendidikan: "S1 Teknik Informatika"
+   
+};
+console.log(anggota);
+
+
+// function hitungluas(){
+  
+// //-->
+
+//     // var alas = document.forms['hitungls3']['alass3'].value;
+//     // alas = parseInt(alas);
+//     // var tinggi = document.forms['hitungls3']['tinggis3'].value;
+//     // tinggi = parseInt(tinggi);
+//     // var luas = (alas*tinggi)/2;
+//     // document.forms['hitungls3']['luass3'].value = luas;
+//     }
+
+// function luaslingkaran() 
+//     { var jari2 = parseFloat(document.funday.nilai_jari2.value);
+//      if(isNaN(jari2)){ 
+//     jari2=0.0;} 
+//     var hasil = 3.14*jari2*jari2; 
+//     alert("Jadi, luas lingkaran adalah : "+ hasil); } 
